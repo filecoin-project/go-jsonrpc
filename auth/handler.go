eb31574bc518daefce6085b5c743c1e74ff843c8
Łukasz Magiera <magik6k@gmail.com>
2020-05-20 20:15:49 +0200

    Bring some auth utils here

---
+++ auth/handler.go
@@ -0,0 +1,48 @@
+package auth
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	logging "github.com/ipfs/go-log/v2"
+)
+
+var log = logging.Logger("auth")
+
+type Handler struct {
+	Verify func(ctx context.Context, token string) ([]Permission, error)
+	Next   http.HandlerFunc
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	token := r.Header.Get("Authorization")
+	if token == "" {
+		token = r.FormValue("token")
+		if token != "" {
+			token = "Bearer " + token
+		}
+	}
+
+	if token != "" {
+		if !strings.HasPrefix(token, "Bearer ") {
+			log.Warn("missing Bearer prefix in auth header")
+			w.WriteHeader(401)
+			return
+		}
+		token = strings.TrimPrefix(token, "Bearer ")
+
+		allow, err := h.Verify(ctx, token)
+		if err != nil {
+			log.Warnf("JWT Verification failed: %s", err)
+			w.WriteHeader(401)
+			return
+		}
+
+		ctx = WithPerm(ctx, allow)
+	}
+
+	h.Next(w, r.WithContext(ctx))
+}
